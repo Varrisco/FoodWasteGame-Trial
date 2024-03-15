985000c7ddd403702eccbdb1392701a00d55493e
Francisco Vasconcelos <49655530+Francisc546@users.noreply.github.com>
2024-03-15 10:06:05 +0000

    omg Francisco you mad crazy (bought UE to GA plugin)

---
+++ Plugins/UEtoGoogleAnalytics/Source/GAComponent/Private/GAComponent.cpp
@@ -0,0 +1,20 @@
+// German Todua. 2024. All Rights Reserved.
+
+#include "GAComponent.h"
+
+#define LOCTEXT_NAMESPACE "FGAComponentModule"
+
+void FGAComponentModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FGAComponentModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FGAComponentModule, GAComponent)
